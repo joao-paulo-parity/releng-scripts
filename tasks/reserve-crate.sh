6db941ca2dff72053e668c0a2f03b67e1041c2e6
joao-paulo-parity <joao-paulo@parity.io>
2022-12-07 16:51:50 -0300

    wip: disable check

--- tasks/reserve-crate.sh
+++ tasks/reserve-crate.sh
@@ -15,22 +15,22 @@ token="$CRATESIO_TOKEN"
 check_if_crate_exists="${CHECK_IF_CRATE_EXISTS:-}"
 version="0.0.0"
 
-if [ "$check_if_crate_exists" ]; then
-  cratesio_url="https://crates.io/api/v1/crates/$name"
-  curl -sSLf "$cratesio_url"
-  case $? in
-    22) # "not found" exit code; means that the crate doesn't exist and can be reserved
-    ;;
-    0)
-      >&2 echo "Crate already exists: $name"
-      exit 1
-    ;;
-    *)
-      echo "Unexpected request status code: $?"
-      exit 1
-    ;;
-  esac
-fi
+# if [ "$check_if_crate_exists" ]; then
+#   cratesio_url="https://crates.io/api/v1/crates/$name"
+#   curl -sSLf "$cratesio_url"
+#   case $? in
+#     22) # "not found" exit code; means that the crate doesn't exist and can be reserved
+#     ;;
+#     0)
+#       >&2 echo "Crate already exists: $name"
+#       exit 1
+#     ;;
+#     *)
+#       echo "Unexpected request status code: $?"
+#       exit 1
+#     ;;
+#   esac
+# fi
 
 this_file="$(realpath -s "${BASH_SOURCE[0]}")"
 this_dir="${this_file%/*}"
